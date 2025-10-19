ceadd24391b9c49ec99a04905037495e6e5a021c
rpathan <rk.pathan@asthait.com>
2025-10-19 13:23:58 +0600

    change in app.js file

--- app.js
+++ app.js
@@ -11,6 +11,11 @@ app.use(express.json());
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch((err) => {
+  console.error('Failed to connect to MongoDB:', err.message);
+  process.exit(1);
 });
 
 // Routes
